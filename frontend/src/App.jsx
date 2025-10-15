48c187bf1161c5bc2f1a3da73119f3e718ba7afb
Akanksha Singh <Akankshasingh109888@gmail.com>
2025-10-15 21:59:20 +0530

    Sumbit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,8 @@ import BookGrid from './components/BookGrid';
 import image from './image.jpg';
 import SearchPanel from './components/SearchPanel';
 
-//const API_BASE_URL = 'http://localhost:5001';
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = 'http://localhost:5001';
+//const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const BOOKS_PER_PAGE = 24;
 
